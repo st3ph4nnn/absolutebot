b4d6c26bb6de27116671c5388fa8d5c395e1923b
st3ph4nnn <stephanarteni@gmail.com>
2022-03-12 09:21:46 -0800

    changes

--- misc/bump.js
+++ misc/bump.js
@@ -1,5 +1,5 @@
 module.exports = function bump(message) {
-    if ( message.embeds[0].description.includes("Check") && message.author.id == '302050872383242240')
+    if (message.author.bot && message.embeds[0].description.includes("Check") && message.author.id == '302050872383242240')
         setTimeout(() => {
             console.log("disboard");
             message.channel.send(`<@&${"916859988507525161"}> please bump`)
