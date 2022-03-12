1002840635535ab3b16432f607a8d6415252dc19
st3ph4nnn <stephanarteni@gmail.com>
2022-03-12 11:39:48 -0800

    fix

--- misc/slurs.js
+++ misc/slurs.js
@@ -2,13 +2,14 @@ var slurs = [ "nigga", "nigger", "nigg", "nig", "n!g", "n!gger", "n!gg" ];
 
 module.exports = function slurs_check(message) {
     msg = message.content.replace(/\s+/g, '').toLowerCase();
-
-    slurs.forEach(slur => {
-        if (slur.includes(msg)) { 
+    var exists = false;
+    for (slur in slurs) {
+        if (msg.includes(slur)) { 
             setTimeout(() => { message.delete(); }, 500); 
-            return true; 
+            exists = true;
+            break;
         }
-    });
+    }
 
-    return false;
+    return exists;
 }
