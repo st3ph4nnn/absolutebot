f73fd4d2a9a2cf85fc896fc34e99411ddab53c79
st3ph4nnn <stephanarteni@gmail.com>
2022-03-12 10:48:09 -0800

    fix

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require('node:fs');
-//const config = require('./config.json');
 const { Client, Intents, Collection } = require("discord.js");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
