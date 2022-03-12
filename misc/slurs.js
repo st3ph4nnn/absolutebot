88d52b998703012ecc3782978f6d7ca5439cbf37
st3ph4nnn <stephanarteni@gmail.com>
2022-03-12 11:48:43 -0800

    fix

--- misc/slurs.js
+++ misc/slurs.js
@@ -2,10 +2,10 @@ var slurs = [ "nigga", "nigger", "nigg", "nig", "n!g", "n!gger", "n!gg" ];
 
 module.exports = function slurs_check(message) {
     msg = message.content.replace(/\s+/g, '').toLowerCase();
-    for (slur in slurs) {
-        if (msg.includes(slur)) { 
-            setTimeout(() => { message.delete(); }, 500); 
+    
+    for (var i = 0; i < slurs.length; i++)
+        if ((msg.toLowerCase().includes(slurs[i]) || msg.includes('discord.gg/'))) {
+            setTimeout(() => {message.delete()}, 500);  
             break;
         }
-    }
 }
